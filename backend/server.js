2fb47ab1c5d34ebbb3a1273472291174a83e6b38
Abhay Singh <78447760+Abhaysingh77@users.noreply.github.com>
2023-12-17 01:38:33 +0530

    updating server.js

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,36 @@
 const express = require("express");
 const cors = require("cors");
-const db='';
-import("lowdb").then((lowDb)=>{
-  db = lowDb(new FileSync("db.json"));
-}).catch((err)=>{
-  console.log(err)
-})
-
+const lowDb = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const bodyParser = require("body-parser");
-const { nanoid} = require("nanoid");
-var dayjs = require("dayjs");
-const utc=require('dayjs/plugin/utc')
-const timezone=require('dayjs/plugin/timezone')
+const { nanoid } = require("nanoid");
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+
 const app = express();
+const PORT = 8000;
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
-dayjs.tz.setDefault("Asia/Kolkata")
+dayjs.tz.setDefault("Asia/Kolkata");
+
+const adapter = new FileSync("db.json");
+const db = lowDb(adapter);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 8000;
-
 app.get("/categories", (req, res) => {
   const data = db.get("categories").value();
-  console.log(data)
+  console.log(data);
   return res.json(data);
 });
 
 app.get("/categories/detail", (req, res) => {
   const data = db.get("categories-detail").value();
-  let response = data.find(ele => {
-    return ele.id == req.query.id;
-  });
+  const response = data.find((ele) => ele.id == req.query.id);
   if (response) return res.json(response);
   else
     return res.status(400).send({
@@ -45,35 +40,30 @@ app.get("/categories/detail", (req, res) => {
 
 app.get("/categories/poses", (req, res) => {
   const data = db.get("pose-detail").value();
-  let response = data.filter((item) => {
-    return item.category_name === req.query.name
-  });
+  const response = data.filter((item) => item.category_name === req.query.name);
 
-  if (response) return res.json(response);
+  if (response.length > 0) return res.json(response);
   else
     return res.status(400).send({
-      message: `Pose not found for ${req.query.id}!`,
+      message: `Pose not found for ${req.query.name}!`,
     });
 });
 
 app.get("/categories/pose/detail", (req, res) => {
   const data = db.get("pose-detail").value();
-  let response = data.find(ele => {
-    return ele.english_name === req.query.name;
-  })
+  const response = data.find((ele) => ele.english_name === req.query.name);
   if (response) return res.json(response);
   else
     return res.status(400).send({
-      message: `Pose not found for ${req.query.id}`
-    })
+      message: `Pose not found for ${req.query.name}`,
+    });
 });
-
 
 app.post("/reservations/new", (req, res) => {
   const reservation = req.body;
   if (!(reservation.name && reservation.email && reservation.phone && reservation.designation)) {
     return res.status(400).send({
-      message: `Invalid data received`,
+      message: "Invalid data received",
     });
   } else {
     reservation.name = reservation.name
@@ -83,7 +73,7 @@ app.post("/reservations/new", (req, res) => {
       .map((i) => i.charAt(0).toUpperCase() + i.slice(1))
       .join(" ");
 
-    const { name, email, phone,designation } = reservation;
+    const { name, email, phone, designation } = reservation;
     db.get("reservations")
       .push({
         name,
@@ -91,8 +81,9 @@ app.post("/reservations/new", (req, res) => {
         phone,
         designation,
         id: nanoid(),
-        time:new Date().toLocaleDateString()
-      }).write();
+        time: new Date().toLocaleDateString(),
+      })
+      .write();
 
     return res.json({ success: true });
   }
@@ -101,6 +92,7 @@ app.post("/reservations/new", (req, res) => {
 app.get("/reservations", (req, res) => {
   const data = db.get("reservations").value();
   if (data) return res.json(data);
+  else return res.json([]); // Send an empty array if there are no reservations
 });
 
 app.listen(process.env.PORT || PORT, () => {
