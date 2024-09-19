b3b15ff6f959061350eda6c73275beb440029d13
Abhay Singh <78447760+Abhaysingh77@users.noreply.github.com>
2024-09-19 19:33:38 +0530

    adding loader

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,28 +1,29 @@
 import config from "../conf/index.js";
 
-const url=`${config.backendEndpoint}/categories`;
+const url = `${config.backendEndpoint}/categories`;
 
-async function init(){
-  const data=await fetchCategoryDetail(url);
-  console.log(data.length)
-  createYogaCategoryCard(data);
-  console.log(window.location)
-} 
+async function init() {
+    const data = await fetchCategoryDetail(url);
+    console.log(data.length)
+    createYogaCategoryCard(data);
+    console.log(window.location)
+}
 
 
-async function fetchCategoryDetail(url){
-    const apiResponse= await fetch(url);
-    const apiData= await apiResponse.json();
+async function fetchCategoryDetail(url) {
+    const apiResponse = await fetch(url);
+    const apiData = await apiResponse.json();
     console.log(apiData);
     return apiData;
 }
 
-function createYogaCategoryCard(data){
-    const categoryContainer=document.getElementById("yoga-card");
+function createYogaCategoryCard(data) {
+    const categoryContainer = document.getElementById("yoga-card");
+    categoryContainer.innerHTML = "";
 
-    for(let i=0; i<data.length; i++){
+    for (let i = 0; i < data.length; i++) {
         console.log("inside loop")
-        categoryContainer.innerHTML+=`<div class="col-lg-3 col-md-4 mt-4 ">
+        categoryContainer.innerHTML += `<div class="col-lg-3 col-md-4 mt-4 ">
             <div class="card text-center category-card" style="">
             <a href="./pages/yoga_page/?name=${data[i].category_name}" class="text-decoration-none text-primary-emphasis m-2" id="${data[i].id}">
             <img src="${data[i].image_url}" class="img-fluid cardImg rounded"  alt="..." />
@@ -38,4 +39,4 @@ function createYogaCategoryCard(data){
     }
     console.log("inside create card func")
 }
-export {init,fetchCategoryDetail,createYogaCategoryCard}
+export { init, fetchCategoryDetail, createYogaCategoryCard }
